0391c8b87ac8c40f92fd0ad70a97f89d29441c39
NallasivamRoobi <nallasivam712@gmail.com>
2021-08-15 11:50:15 +0530

    new coo=mmit

---
+++ src/org/cts/tst/emp/EmployeeDetails.java
@@ -0,0 +1,30 @@
+package org.cts.tst.emp;
+
+public class EmployeeDetails {
+
+	    public static void main(String[] args) {
+			
+	    	Short empid=12;
+	    	System.out.println(empid);
+	    	
+	    	String empName="Nallasivam";
+	    	System.out.println("Employee name is"+"  "+empName);
+	    	
+	    	String empMail="nallasivam202@gmail.com";
+	    	System.out.println("Maild is:"+"  "+empMail);
+	    	
+	    	Long empPhoneNo=9245273446L;
+	    	System.out.println("Employee Phone no."+"  "+empPhoneNo);
+	    	
+	    	double empSalary=98000.50;
+	    	System.out.println("Emplyee salary is "+"  "+empSalary);
+	    	
+	    	char empGender='M';
+	    	System.out.println("Employee Gender is MAle " + "  " +empGender);
+	    	
+	    	String empCity="Chennai";
+	    	System.out.println("Employee lives in"+"   "+empCity);
+	    	
+		}
+	    
+}
---
+++ src/org/cts/tst/emp/Sample33.java
@@ -0,0 +1,30 @@
+package org.cts.tst.emp;
+
+public class Sample33 {
+	
+	public static void main(String[] args) {
+		
+		int a=10; int b=20; int c=9;
+		if((a>b)
+		{
+			if(a>c)
+			{
+				System.out.println("a is bif);
+			}
+			
+		}
+		
+		else if(b>a)
+		{
+			if(b>c)
+			{
+				System.out.println("bis big");
+						
+			}
+		}
+		
+		else
+		System.out.println("after if");
+	}
+
+}
